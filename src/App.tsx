159692aedbe2e904b6ef220a5f1a57a5e0a9d64a
sholikul <sholikulardian@gmail.com>
2024-10-28 10:39:17 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Users } from './components/Users';
+import { Posts } from './components/Posts';
+
+function App() {
+  return (
+    <>
+      <Users />
+      <Posts />
+    </>
+  );
+}
+
+export default App;
