15410ffcfe50f4c48252128a5131df7cbc6f0e48
sholikul <sholikulardian@gmail.com>
2024-10-29 12:36:17 +0700

    implementation caching with localstorage

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -9,17 +9,29 @@ export const useFetch = <T = DefaultApiResponse>(url: string) => {
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
+  const { pathname } = new URL(url);
+  const path = pathname.replace('/', '-');
+
   useEffect(() => {
     const fetchingData = async () => {
       try {
-        const response = await fetch(url);
+        const cacheKey = `cache${path}`;
+        const cacheData = localStorage.getItem(cacheKey);
 
-        if (!response.ok) {
-          throw new Error('failed to fetching data');
+        if (cacheData) {
+          setData(JSON.parse(cacheData));
+        } else {
+          const response = await fetch(url);
+
+          if (!response.ok) {
+            throw new Error('failed to fetching data');
+          }
+
+          const result = await response.json();
+          setData(result);
+
+          localStorage.setItem(cacheKey, JSON.stringify(result));
         }
-
-        const result = await response.json();
-        setData(result);
       } catch (err) {
         if (err instanceof Error) {
           setError(err.message);
@@ -30,7 +42,7 @@ export const useFetch = <T = DefaultApiResponse>(url: string) => {
     };
 
     fetchingData();
-  }, [url]);
+  }, [url, path]);
 
   return { data, loading, error };
 };
