862f1c6eedf3040f59213b7a2cdb624c5a514c93
sholikul <sholikulardian@gmail.com>
2024-10-29 12:34:16 +0700

    implementation route

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-// import './index.css'
-import App from './App.tsx';
+import { BrowserRouter } from 'react-router-dom';
+import { RouterProvider } from './routes.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <BrowserRouter>
+    <RouterProvider />
+  </BrowserRouter>
 );
